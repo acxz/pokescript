862bf367841c42e7ee684b315b9af53a2033769a
Bart Duisters <bartduisters@bartduisters.com>
2021-08-11 21:30:33 +0200

    Add Mac support

---
+++ install-mac.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# A basic install script for pokemon-colorscripts
+
+INSTALL_DIR='/usr/local/opt'
+BIN_DIR='/usr/local/bin'
+
+# deleting directory if it already exists
+rm -rf $INSTALL_DIR/pokemon-colorscripts || return 1
+
+# making the necessary folder structure
+mkdir -p $INSTALL_DIR/pokemon-colorscripts || return 1
+
+# moving all the files to appropriate locations
+cp -rf colorscripts $INSTALL_DIR/pokemon-colorscripts
+cp pokemon-colorscripts-mac.sh $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.sh
+cp nameslist.txt $INSTALL_DIR/pokemon-colorscripts
+
+# create symlink in usr/bin
+rm -rf $BIN_DIR/pokemon-colorscripts || return 1
+ln -s $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.sh $BIN_DIR/pokemon-colorscripts
+
