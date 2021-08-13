2b99fc8eef135ccc3590bb950786b9079be5ceb3
Bart Duisters <bartduisters@bartduisters.com>
2021-08-13 20:25:07 +0200

    default to non-coreutils option

--- pokemon-colorscripts.sh
+++ pokemon-colorscripts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 OS=$(uname)
-test $OS == 'Linux' && PROGRAM=$(readlink -f "$0")
+PROGRAM=$(readlink -f "$0")
 test $OS == 'Darwin' && PROGRAM=$(greadlink -f "$0")
 
 PROGRAM_DIR=$(dirname "$PROGRAM")
@@ -35,7 +35,7 @@ _show_random_pokemon(){
     NUM_ART=$(ls -1 "$POKEART_DIR"|wc -l)
     # getting a random index from 0-NUM_ART. (using shuf instead of $RANDOM for POSIX compliance)
 
-    test $OS == 'Linux' && random_index=$(shuf -i 1-"$NUM_ART" -n 1)
+    random_index=$(shuf -i 1-"$NUM_ART" -n 1)
     test $OS == 'Darwin' && random_index=$(gshuf -i 1-"$NUM_ART" -n 1)
 
     random_pokemon=$(sed $random_index'q;d' "$PROGRAM_DIR/nameslist.txt")
